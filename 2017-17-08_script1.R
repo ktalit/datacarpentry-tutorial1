c888a37643b2b4c386d85488322f9f75e96474bb
ktalit <ktalit@merced.ucmerced.edu>
2017-08-18 09:49:20 -0700

    added data and script

---
+++ 2017-17-08_script1.R
@@ -0,0 +1,60 @@
+## my first R_csript
+## Kuntal ktalit@ucmerced.edu
+
+
+##download data -------
+# download.file("https://ndownloader.figshare.com/files/2292169",
+#               "data/portal_data_joined.csv")
+
+
+## read in data into R----
+ surveys <-read.csv("data/portal_data_joined.csv")
+#explore our data
+ head(surveys)  #show first 6 rows 
+ tail(surveys)  #show last 6 rows
+ tail(surveys, 12) #show last 12 rows
+ ##summaries
+ str(surveys) #structure of the data
+ summary(surveys)
+ 
+ ##explore size
+ dim(surveys)
+ 
+ names(surveys)
+ 
+ 
+ ## the $ operator for isolating collumns
+ tail(surveys$weight)
+ str(surveys$weight)
+ summary(surveys$weight)
+ 
+ ## plot
+ plot(surveys$year,surveys$weight)
+ plot(surveys$hindfoot_length,surveys$weight)
+ 
+ ## explore month
+ summary(surveys$month)
+ hist(surveys$month)
+ 
+ ## explore factor variable
+ summary(surveys$taxa)
+ levels(surveys$taxa)
+ nlevels(surveys$taxa)
+ #hist(surveys$taxa) #error
+ class(surveys$taxa) #turns it into a table
+ class(table(surveys$taxa))
+ barplot(table(surveys$taxa))
+ 
+ ## subset in base R----
+ ##[rows,columns]
+ ## return all the collumns for genus Ammodromus
+ surveys[surveys$genus=='Ammodramus',]
+ 
+ ##return afew columns
+ #surveys[survey$genus == 'Ammodramus', c('record_id','month',"weight")]
+ ##display count for month Jan and Feb---
+ summary(surveys$month==1 | surveys$month==2)
+ summary(surveys$month==2)
+
+  
+ 
